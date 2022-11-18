7bd61b7f891a37c275ec322d298a8ff5f597392d
Fourcade7 <fourcadeaminov7@gmail.com>
2022-11-18 22:02:36 +0500

    text Customization

--- app/src/main/java/com/pr7/jetpack_compose/JC_4_Text_Customization.kt
+++ app/src/main/java/com/pr7/jetpack_compose/JC_4_Text_Customization.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -76,7 +77,7 @@ fun pr4(){
             append("C")
             append("D")
         })
-        Text(text = "Axaxaxa".repeat(40))
+        Text(text = "Axaxaxa".repeat(40), maxLines = 4, overflow = TextOverflow.Clip)
 
     }
 }
