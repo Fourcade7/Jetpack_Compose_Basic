07bf337265fafe102df364cb9dd174aeea6cdf0a
Fourcade7 <fourcadeaminov7@gmail.com>
2022-11-18 22:00:15 +0500

    text Customization

--- app/src/main/java/com/pr7/jetpack_compose/JC_4_Text_Customization.kt
+++ app/src/main/java/com/pr7/jetpack_compose/JC_4_Text_Customization.kt
@@ -3,6 +3,7 @@ package com.pr7.jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,8 +11,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pr7.jetpack_compose.ui.theme.Jetpack_ComposeTheme
+import com.pr7.jetpack_compose.ui.theme.PrCustomColor
 
 class JC_4_Text_Customization : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +39,52 @@ class JC_4_Text_Customization : ComponentActivity() {
         }
     }
 }
+
+@Preview(showBackground =true)
+@Composable
+fun pr4(){
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "Uaxaxa",
+            color=Color.White,
+
+
+            modifier = Modifier
+                .padding(35.dp)
+                .background(PrCustomColor)
+                .padding(25.dp),
+            fontSize = 18.sp,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.Bold
+
+
+        )
+
+        Text(buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    fontSize = 35.sp,
+                    color = PrCustomColor
+                ),
+
+
+            ){
+                append("A")
+            }
+
+            append("B")
+            append("C")
+            append("D")
+        })
+        Text(text = "Axaxaxa".repeat(40))
+
+    }
+}
+
+
+
+
+
 
 
 
